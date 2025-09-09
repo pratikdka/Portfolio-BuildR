bc98041815d6787a1e9fb74a67a9ed5b5a1125d4
Pratik Khadka <pratik.khadka18@gmail.com>
2025-09-09 14:26:49 +1000

    Initialized Express JS Project and basic folder structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
