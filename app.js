cc4c2cff8e32427ed3c743c19e03bfd7dcb18413
Abhishek Acharya <acharyasafal12466@gmail.com>
2025-09-09 15:52:23 +1000

    Designed UI Form

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/form", (req, res) => {
+  res.render("form");
 });
 
 const PORT = process.env.PORT || 3000;
